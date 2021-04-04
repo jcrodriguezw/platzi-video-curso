bb17b2f2ffc1416fcb83d7427565a4c3c4724215
Juan C. Rodriguez W <jcrodriguezw@gmail.com>
2021-04-04 09:41:50 -0500

    first commit with ESlint

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/App.scss';
+
+const App = () => {
+    return (
+        <div>
+            <p>Hola</p>
+        </div>
+    )
+}
+export default App;
